2d142dc6038cda454bd26afe48c1cdb668053f54
Rebeca <rcks.santos@gmail.com>
2022-03-14 18:56:55 -0300

    🎬 começou o bolão do Oscarvore :D

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './docs/index.html',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+    },
+  },
+  plugins: [],
+}
