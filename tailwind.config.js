a13cff932e6eab0667b823c4f8b2d165b6a0c4b2
Rebeca <rcks.santos@gmail.com>
2022-03-14 20:52:14 -0300

    🎞️ estrutura inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
     },
+    extend: {
+      colors: {
+        livros: '#45d0c1',
+        preto6: '#2d2d2e',
+      },
+    }
   },
   variants: {
     extend: {
